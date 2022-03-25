6f7c176a24503c7a97369cc700bca29d3bb8ade4
Violetta Petrova <vwondercame@gmail.com>
2022-03-25 13:02:33 -0400

    create and setup for singular feedback item

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Header from './components/Header';
+import FeedbackItem from './components/FeedbackItem';
 
 function App() {
   return (
     <>
       <Header />
-      <div className='container'>
-        <h1>My App</h1>
-      </div>
+      <div className='container'></div>
+      <FeedbackItem/>
     </>
   );
 }
