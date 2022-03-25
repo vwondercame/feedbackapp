4d0cc5f0e49824cdbd790b8b4f80180685cd3ac6
Violetta Petrova <vwondercame@gmail.com>
2022-03-25 13:06:01 -0400

    create FeedbackData file with an array of feedback rating and comments

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackItem from './components/FeedbackItem';
 
 function App() {
+  const [feedback, setFeedback] = useState()
   return (
     <>
       <Header />
