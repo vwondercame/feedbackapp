7cb54089b96d7bf7a3ea506490c400f80f9f5fb1
Violetta Petrova <vwondercame@gmail.com>
2022-03-25 12:19:38 -0400

    setup for the header element

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types'
+
+function Header( { text } ) {
+    const headerStyles = {
+        backgroundColor: bgColor,
+        color: textColor,
+    }
+
+  return (
+    <header style={headerStyles}>
+        <div className="container">
+            <h2>{text}</h2>
+        </div>
+    </header>
+  )
+}
+
+Header.defaultProps = {
+    text: 'Feedback UI',
+    bgColor: 'rgba(0,0,0)'
+}
+
+Header.propTypes = {
+    text: PropTypes.string,
+}
+
+export default Header;
